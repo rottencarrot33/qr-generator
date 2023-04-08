1ca9cecfbf4bde6ccd16b3a975e5b422555479eb
rottencarrot33 <122114827+rottencarrot33@users.noreply.github.com>
2023-04-08 03:18:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const form = document.getElementById('generate-form');
+const qr = document.getElementById('qrcode');
+
+const onGenerateSubmit = (e) => {
+    e.preventDefault();
+
+    clearUI();
+
+    const url = document.getElementById('url').value;
+    const size = document.getElementById('size').value;
+
+    if (url === '') {
+        alert('Please enter a URL');
+    } else {
+        showSpinner();
+
+        setTimeout(() => {
+            hideSpinner();
+
+            generateQRCode(url, size);
+
+            setTimeout(() => {
+                const saveUrl = qr.querySelector('img').src;
+
+                createSaveBtn(saveUrl);
+            }, 50);
+        }, 1000);
+    }
+};
+
+const generateQRCode = (url, size) => {
+    const qrcode = new QRCode('qrcode', {
+        text: url,
+        width: size,
+        height: size,
+    });
+};
+
+const clearUI = () => {
+    qr.innerHTML = '';
+    const saveBtn = document.getElementById('save-link');
+    if (saveBtn) {
+        saveBtn.remove();
+    }
+};
+
+const showSpinner = () => {
+    document.getElementById('spinner').style.display = 'block';
+};
+
+const hideSpinner = () => {
+    document.getElementById('spinner').style.display = 'none';
+};
+
+const createSaveBtn = (saveUrl) => {
+    const link = document.createElement('a');
+    link.id = 'save-link';
+    link.classList =
+        'btn-success btn my-4';
+    link.href = saveUrl;
+    link.style = 'width:300px'
+    link.download = 'qrcode';
+    link.innerHTML = 'Save Image';
+    document.getElementById('generated').appendChild(link);
+};
+
+hideSpinner();
+
+form.addEventListener('submit', onGenerateSubmit);
